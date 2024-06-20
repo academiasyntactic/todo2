e6d6d196795bf3051fd80e87c8d6c24599e8de34
academiasyntactic <syntacticacademia@gmail.com>
2024-06-20 07:29:37 -0500

    eliminar

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,16 @@ const App = () => {
     ]);
   };
 
+  const eliminarTarea = (identificacion) => {
+    const nuevasTareas = tareas.filter(
+      (elemento) => elemento.id !== identificacion
+    );
+    setTareas(nuevasTareas);
+  };
+
   return (
     <div>
-      <div className="phone-1 flex">
+      <div className="phone-1 flex justify-center items-center">
         <label className="input input-bordered flex items-center gap-2">
           <input
             onChange={input1}
@@ -32,28 +39,38 @@ const App = () => {
         </button>
       </div>
 
-      {tareas &&
-        tareas.map((elemento) => (
-          <div key={elemento.id} className="flex gap-10">
-            <h1 className="text-[30px]">{elemento.descripcion}</h1>
-            <button className="btn btn-circle btn-outline">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      <div className="flex flex-col items-center justify-center">
+        {tareas &&
+          tareas.map((elemento) => (
+            <div
+              key={elemento.id}
+              className="flex gap-10 border border-green-500 rounded-md w-fit px-3 py-2 mt-2 justify-center items-center"
+            >
+              <h1 className="text-[30px]">{elemento.descripcion}</h1>
+              <button
+                onClick={() => {
+                  eliminarTarea(elemento.id);
+                }}
+                className="btn btn-circle btn-outline"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-        ))}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
